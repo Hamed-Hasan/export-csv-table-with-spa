7f2172e3ffab586198b68ba086206bf512b25c1d
John Doe <hamedhasan422421@gmail.com>
2022-06-29 20:39:15 +0300

    table & blog page added

---
+++ src/Components/Blog/Blog.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Blog = () => {
+    return (
+        <div>
+          <h1 className="font-bold underline text-3xl">This Is Blog</h1>
+        </div>
+    );
+};
+
+export default Blog;
---
+++ src/Components/Table/Table.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Table = () => {
+    return (
+        <div>
+            <h1 className="font-bold underline text-3xl">This Is Table</h1>
+        </div>
+    );
+};
+
+export default Table;
