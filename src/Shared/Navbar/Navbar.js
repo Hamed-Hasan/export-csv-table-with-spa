123a3afb0962a99682482c29fde0ebae529329cb
John Doe <hamedhasan422421@gmail.com>
2022-06-29 20:32:30 +0300

    setup navbar active route with spa

--- src/Shared/Navbar/Navbar.js
+++ src/Shared/Navbar/Navbar.js
@@ -1,28 +1,28 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
 
    
 
     return (
-        <div class="navbar bg-base-100">
+        <div class="navbar bg-base-100 px-12">
   <div class="navbar-start">
     <div class="dropdown">
       <label tabindex="0" class="btn btn-ghost lg:hidden">
         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
       </label>
       <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-        <li><a>Table</a></li>
-        <li><a>Blog</a></li>
+        <NavLink><a>Table</a></NavLink>
+        <NavLink><a>Blog</a></NavLink>
       </ul>
     </div>
-    <a class="btn btn-ghost normal-case text-xl">daisyUI</a>
+    <a class="btn btn-ghost normal-case text-xl">ExPorT WiTh SPA</a>
   </div>
   <div class="navbar-end hidden lg:flex">
     <ul class="menu menu-horizontal p-0">
-      <li><a>Table</a></li>
-    
-      <li><a>Table</a></li>
+      <NavLink><a>Table</a></NavLink>
+      <NavLink><a>Table</a></NavLink>
     </ul>
   </div>
 </div>
