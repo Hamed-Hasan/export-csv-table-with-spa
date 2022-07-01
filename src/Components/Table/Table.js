970564666ce627a4c65605c1587c92aea39e2cb8
John Doe <hamedhasan422421@gmail.com>
2022-07-01 23:09:48 +0300

    using data structure

--- src/Components/Table/Table.js
+++ src/Components/Table/Table.js
@@ -4,7 +4,15 @@ const Table = () => {
     const [row, setRow] = useState(3);
     const [obj, setObj] = useState({});
     let custs = [];
-
+    for (let i = 0; i <= row; i++) {
+        custs.push({
+            firstName: `first${i}`,
+            lastName: `last${i}`,
+            email: `email${i}`,
+            address: `address${i}`,
+            zipCode: `zipCode${i}`,
+        });
+    }
     return (
         <div>
             <h1 className="font-bold underline text-3xl">This Is Table</h1>
