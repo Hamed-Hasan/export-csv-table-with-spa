caed1abc11d8387b8ff7f0bd9ebc683ae3bac4c0
John Doe <hamedhasan422421@gmail.com>
2022-07-01 23:27:38 +0300

    using alghorithom

--- src/Components/Table/Table.js
+++ src/Components/Table/Table.js
@@ -14,13 +14,54 @@ const Table = () => {
         });
     }
     return (
-        <div>
-            <h1 className="font-bold underline text-3xl">This Is Table</h1>
+        <div className='container mx-auto'>
+            <h1 className="font-bold underline text-3xl">User Data</h1>
 
             <div className="mt-14 px-14">
+            <div class="overflow-x-auto">
+  <table class="table w-full">
+  
+    <thead>
+      <tr>
+        <th></th>
+        <th>Name</th>
+        <th>Job</th>
+        <th>Favorite Color</th>
+        <th>Favorite Color</th>
+        <th>Favorite Color</th>
+      </tr>
+    </thead>
+    <tbody>
+
+     {
+        custs.map((single, index) => {
+            const changeFirstName = (e) => {
+                custs[index].firstName = e.target.value;
+            };
+            const changeLastName = (e) => {
+                custs[index].lastName = e.target.value;
+            };
+            const changeEmail = (e) => {
+                custs[index].email = e.target.value;
+            };
+            const changeAddress = (e) => {
+                custs[index].address = e.target.value;
+            };
+            const changeZipCode = (e) => {
+                custs[index].zipCode = e.target.value;
+            };
+        })
+     }
    
-
-
+      <tr>
+        <th>3</th>
+        <td>Brice Swyre</td>
+        <td>Tax Accountant</td>
+        <td>Red</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
 
             </div>
         </div>
